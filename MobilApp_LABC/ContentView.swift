738be17739ac5ea123dc8078dc1639845721f42a
Ernst Reutergårdh <kedre@kth.se>
2020-12-16 13:14:22 +0100

    Initial Commit

---
+++ MobilApp_LABC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobilApp_LABC
+//
+//  Created by Ernst on 2020-12-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobilApp_LABC/MobilApp_LABCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilApp_LABCApp.swift
+//  MobilApp_LABC
+//
+//  Created by Ernst on 2020-12-16.
+//
+
+import SwiftUI
+
+@main
+struct MobilApp_LABCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
