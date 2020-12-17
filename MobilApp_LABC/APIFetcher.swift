098643f884ce3a8746cdb49de583f88818ba0ce3
carl-bernhardhallberg <cbha@kth.se>
2020-12-17 19:21:14 +0100

    - Skapat ett API Call

--- MobilApp_LABC/APIFetcher.swift
+++ MobilApp_LABC/APIFetcher.swift
@@ -7,7 +7,20 @@
 
 import Foundation
 
+let myNotificationKey = "se.challberg.myNotificationKey"
 
 func fetchFromApi() -> Void{
+    
+    let web = "https://openstreetgs.stockholm.se/geoservice/api/8899780-10f5-430b-ac3b-9022a45d3d48/wfs?service=WFS&version=1.1.0&request=DescribeFeaturetype&typeName=od%3AParkeringsautomat"
+    guard let url = URL(string: web) else { return }
+    let req = URLRequest(url: url)
+    URLSession.shared.dataTask(with: req) { data, response, error in
+        if let data = data{
+            if let jsonString = String(data: data, encoding: .utf8) {
+                print(jsonString)
+             }
+            //NotificationCenter.default.post(name: Notification.Name(rawValue: myNotificationKey), object: nil)
+        }
+    }.resume()
     
 }
