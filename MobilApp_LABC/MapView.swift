ffce6531b53973a8827000213dd5927575f3539d
Ernst Reutergårdh <kedre@kth.se>
2020-12-22 19:41:32 +0100

    Man kan nu trycka på en annotation och få ut den specifika annotationens information

--- MobilApp_LABC/MapView.swift
+++ MobilApp_LABC/MapView.swift
@@ -14,26 +14,50 @@ struct MapView: UIViewRepresentable{
      https://www.hackingwithswift.com/books/ios-swiftui/communicating-with-a-mapkit-coordinator
      */
     class Coordinator:NSObject, MKMapViewDelegate{
-        var parent: MapView
+        private var parent: MapView
+        private var selectedAnnotation:PAutomat?
         init(_ parent:MapView) {
             self.parent = parent
         }
-        
+        /*
+         Function to manipulate a annotations appearance on the MapView
+         https://www.codementor.io/@muneebali/mapkit-map-pin-and-annotation-11l2dkqvl2
+         */
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let identifier = "Placemark"
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-            if(annotationView == nil){
-                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-                annotationView?.canShowCallout = true
-                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            let Identifier = "Pin"
+               let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Identifier) ?? MKAnnotationView(annotation: annotation, reuseIdentifier: Identifier)
+            
+               annotationView.canShowCallout = true
+               if annotation is MKUserLocation {
+                  return nil
+               } else if annotation is MapPin {
+                  //annotationView.image =  UIImage(imageLiteralResourceName: "Pin")
+                  return annotationView
+               } else {
+                  return nil
+               }
             }
-            else{
-                annotationView?.annotation = annotation
-            }
-            return annotationView
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            
+            selectedAnnotation = view.annotation as? PAutomat
+            print(selectedAnnotation?.adress)
+            print(selectedAnnotation?.status)
         }
- 
- 
+/*
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView){
+            if view.annotation is MKUserLocation {
+                return
+            }
+
+            let ibp = view.annotation as? PAutomat
+            print(ibp!.adress)
+            /*
+            ibp_id = (ibp?.court_id)!
+            IdentifierCell = "pin_IBP"
+            performSegue(withIdentifier: "map_ibp_info", sender: self)
+ */
+        }
+ */
     }
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -55,13 +79,7 @@ struct MapView: UIViewRepresentable{
         mapView.delegate = context.coordinator
         return mapView
     }
-    
     func updateUIView(_ uiView: MKMapView, context: Context) {
         uiView.addAnnotations(automater)
-    }
-}
-
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {        MapView(automater: [])
     }
 }
